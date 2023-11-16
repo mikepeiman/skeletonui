fc105dc1fb086e6f5ed6da85d15d2576d80d5d60
Mike Peiman <mikepeiman@gmail.com>
2023-11-16 16:46:29 -0500

    framing out the AppShell layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar, AppRail } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	let currentTile: number = 0;
+	let icon;
 </script>
 
 <!-- App Shell -->
@@ -18,33 +20,58 @@
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
-				class="btn btn-sm variant-ghost-surface"
-				href="https://discord.gg/EXqV7W8MtY"
-				target="_blank"
-				rel="noreferrer"
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
 				>
-				Discord
+					Discord
 				</a>
 				<a
-				class="btn btn-sm variant-ghost-surface"
-				href="https://twitter.com/SkeletonUI"
-				target="_blank"
-				rel="noreferrer"
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
 				>
 					Twitter
 				</a>
 				<a
-				class="btn btn-sm variant-ghost-surface"
-				href="https://github.com/skeletonlabs/skeleton"
-				target="_blank"
-				rel="noreferrer"
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
 				>
-				GitHub
-			</a>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+		<!-- Page Route Content -->
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+
+	
+	<AppRail>
+		<svelte:fragment slot="lead">
+			<AppRailAnchor href="/">(icon)</AppRailAnchor>
 		</svelte:fragment>
-	</AppBar>
-</svelte:fragment>
-<!-- <AppRail >AppRail</AppRail> -->
-	<!-- Page Route Content -->
-	<slot />
+		<!-- --- -->
+		<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<span>Tile 1</span>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<span>Tile 2</span>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			<span>Tile 3</span>
+		</AppRailTile>
+		<!-- --- -->
+		<svelte:fragment slot="trail">
+			<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+		</svelte:fragment></AppRail
+		>
+	</svelte:fragment>
+		<slot />
 </AppShell>
