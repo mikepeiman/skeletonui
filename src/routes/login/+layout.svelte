f73b2e00ce8eac923cb3d4d91aabba47d5f23603
Mike Peiman <mikepeiman@gmail.com>
2023-11-17 18:23:10 -0500

    cleaned up code, added page transitions with key

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,79 +1,12 @@
 <script lang="ts">
-	import { Step, Stepper } from '@skeletonlabs/skeleton';
-	import { Card } from 'flowbite-svelte';
-	import { page } from '$app/stores';
+	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
+import { Card } from 'flowbite-svelte';
 
-	function onCompleteHandler(e: Event): void {
-		console.log('event:complete', e);
-	}
-	function onStepHandler(e: {
-		detail: { state: { current: number; total: number }; step: number };
-	}): void {
-		console.log('event:step', e);
-	}
-
-
-	const steps = [
-		{
-			header: 'Step 1',
-			content: 'content',
-			href: '/login',
-		},
-		// fill in for remaining steps
-		{
-			header: 'Step 2',
-			content: 'content',
-			href: '/login/username',
-		},
-		{
-			header: 'Step 3',
-			content: 'content',
-			href: '/login/photo',
-		},
-		{
-			header: 'Step 4',
-			content: 'content',
-		},
-		{
-			header: 'Step 5',
-			content: 'content',
-		}
-	]
 </script>
 
-<Stepper on:complete={onCompleteHandler} on:step={onStepHandler}>
-	{#each steps as step}
-		<Step>
-			<svelte:fragment slot="header">{step.header}</svelte:fragment>
-			<a href="{step.href}">{step.content}</a>	
-			{$page.route.id}
 
-		</Step>
-	{/each}
-	<!-- 
-	<Step class="p-4 rounded-lg bg-cyan-400">
-		<svelte:fragment slot="header">(header)</svelte:fragment>
-		(content)
-	</Step>
-	<Step>
-		<svelte:fragment slot="header">(header)</svelte:fragment>
-		(content)
-	</Step>
-	<Step>
-		<svelte:fragment slot="header">(header)</svelte:fragment>
-		(content)
-	</Step>
-	<Step>
-		<svelte:fragment slot="header">(header)</svelte:fragment>
-		(content)
-	</Step>
-	<Step>
-		<svelte:fragment slot="header">(header)</svelte:fragment>
-		(content)
-	</Step> -->
-	<!-- ... -->
-</Stepper>
-
-<Card>
-	<slot>Slot Fallback in Login Layout</slot>
+<AnimatedRoute>
+<Card class="bg-blue-600">
+	<slot />
 </Card>
+</AnimatedRoute>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,43 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+	import { Step, Stepper } from '@skeletonlabs/skeleton';
+	import { Card } from 'flowbite-svelte';
+	import { page } from '$app/stores';
+
+	function onCompleteHandler(e: Event): void {
+		console.log('event:complete', e);
+	}
+	function onStepHandler(e: {
+		detail: { state: { current: number; total: number }; step: number };
+	}): void {
+		console.log('event:step', e);
+	}
+
+	const steps = [
+		{
+			header: 'Step 1',
+			content: 'content',
+			href: '/login'
+		},
+		// fill in for remaining steps
+		{
+			header: 'Step 2',
+			content: 'content',
+			href: '/login/username'
+		},
+		{
+			header: 'Step 3',
+			content: 'content',
+			href: '/login/photo'
+		}
+	];
 </script>
 
-<h1>Login</h1>
+<Stepper on:complete={onCompleteHandler} on:step={onStepHandler}>
+	{#each steps as step}
+		<Step>
+			<svelte:fragment slot="header">{step.header}</svelte:fragment>
+		</Step>
+	{/each}
+</Stepper>
+
+<slot />
