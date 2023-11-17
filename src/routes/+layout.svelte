69c2d38d4e31f8f24a47a34f7660b98b7b5b9c78
Mike Peiman <mikepeiman@gmail.com>
2023-11-17 16:32:36 -0500

    refactoring AppRail into data object and #each block

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,27 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	let currentTile: number = 0;
 	let icon;
+
+	let railItems = [
+		{
+			value: 0,
+			href: '/login',
+			title: 'item-1',
+			slot: 'lead',
+		},
+		{
+			value: 0,
+			href: '/login/username',
+			title: 'item-2',
+			slot: 'lead',
+		},
+		{
+			value: 0,
+			href: '/login/photo',
+			title: 'item-3',
+			slot: 'lead',
+		},
+	]
 </script>
 
 <!-- App Shell -->
@@ -48,30 +69,31 @@
 		<!-- Page Route Content -->
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<svelte:fragment slot="lead">
+				<AppRailAnchor href="/">Home</AppRailAnchor>
+			</svelte:fragment>
+			<!-- --- -->
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+				<AppRailAnchor href="/login">
+					<svelte:fragment slot="lead">Login</svelte:fragment></AppRailAnchor
+				>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+				<AppRailAnchor href="/login">
+					<svelte:fragment slot="lead">Login</svelte:fragment></AppRailAnchor
+				>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+				<AppRailAnchor href="/login">
+					<svelte:fragment slot="lead">Login</svelte:fragment></AppRailAnchor
+				>
+			</AppRailTile>
 
-	
-	<AppRail>
-		<svelte:fragment slot="lead">
-			<AppRailAnchor href="/">(icon)</AppRailAnchor>
-		</svelte:fragment>
-		<!-- --- -->
-		<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
-			<svelte:fragment slot="lead">(icon)</svelte:fragment>
-			<span>Tile 1</span>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-			<svelte:fragment slot="lead">(icon)</svelte:fragment>
-			<span>Tile 2</span>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
-			<svelte:fragment slot="lead">(icon)</svelte:fragment>
-			<span>Tile 3</span>
-		</AppRailTile>
-		<!-- --- -->
-		<svelte:fragment slot="trail">
-			<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
-		</svelte:fragment></AppRail
+			<svelte:fragment slot="trail">
+				<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+			</svelte:fragment></AppRail
 		>
 	</svelte:fragment>
-		<slot />
+	<slot />
 </AppShell>
--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Step, Stepper } from '@skeletonlabs/skeleton';
-
 	import { Card } from 'flowbite-svelte';
+	import { page } from '$app/stores';
 
 	function onCompleteHandler(e: Event): void {
 		console.log('event:complete', e);
@@ -11,9 +11,46 @@
 	}): void {
 		console.log('event:step', e);
 	}
+
+
+	const steps = [
+		{
+			header: 'Step 1',
+			content: 'content',
+			href: '/login',
+		},
+		// fill in for remaining steps
+		{
+			header: 'Step 2',
+			content: 'content',
+			href: '/login/username',
+		},
+		{
+			header: 'Step 3',
+			content: 'content',
+			href: '/login/photo',
+		},
+		{
+			header: 'Step 4',
+			content: 'content',
+		},
+		{
+			header: 'Step 5',
+			content: 'content',
+		}
+	]
 </script>
 
 <Stepper on:complete={onCompleteHandler} on:step={onStepHandler}>
+	{#each steps as step}
+		<Step>
+			<svelte:fragment slot="header">{step.header}</svelte:fragment>
+			<a href="{step.href}">{step.content}</a>	
+			{$page.route.id}
+			{$page.route.id?.match(/^\/login\/.+/) ? 'true' : 'false'}
+		</Step>
+	{/each}
+	<!-- 
 	<Step class="p-4 rounded-lg bg-cyan-400">
 		<svelte:fragment slot="header">(header)</svelte:fragment>
 		(content)
@@ -33,7 +70,7 @@
 	<Step>
 		<svelte:fragment slot="header">(header)</svelte:fragment>
 		(content)
-	</Step>
+	</Step> -->
 	<!-- ... -->
 </Stepper>
 
