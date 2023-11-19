fbc7b076c03c92805a148a04cabd504f0ab4541d
Mike Peiman <mikepeiman@gmail.com>
2023-11-19 18:58:10 -0500

    added links to bio page

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,39 +1,44 @@
 <script lang="ts">
-	import UserLink from "$lib/components/UserLink.svelte";
-    import type { PageData } from "./$types";
+	import UserLink from '$lib/components/UserLink.svelte';
+	import type { PageData } from './$types';
 
-    import imgjpg from "$lib/assets/user.jpg";
-    export let data: PageData;
-    console.log(`🚀 ~ file: +page.svelte:6 ~ data:`, data)
-  
+	import imgjpg from '$lib/assets/user.jpg';
+	import { Card } from 'flowbite-svelte';
+	export let data: PageData;
+	console.log(`🚀 ~ file: +page.svelte:6 ~ data:`, data);
 </script>
-  
-  <svelte:head>
-    <title>@{data.username} Links</title>
-    <meta name="description" content={data.bio} />
-  </svelte:head>
-  
-  
-  <main class="prose text-center mx-auto mt-8">
 
-    <h1 class="text-7xl text-purple-500">
-      @{data.username}
-    </h1>
-  
-    <img
-      src={data.photoURL ?? imgjpg}
-      alt="photoURL"
-      width="256"
-      class="mx-auto"
-    />
-  
-    <p class="text-xl my-8">{data.bio ?? "no bio yet..."}</p>
-    <ul class="list-none">
-      {#each data.links as item}
-        <li>
-          <UserLink {...item} />
-        </li>
-      {/each}
-    </ul>
-  
-  </main>
+<svelte:head>
+	<title>@{data.username} Links</title>
+	<meta name="description" content={data.bio} />
+</svelte:head>
+
+<main class="center justify-center text-center mx-auto mt-8 flex w-full">
+<div class="flex flex-col w-screen">
+        
+            <h1 class="text-7xl text-purple-500">
+                @{data.username}
+            </h1>
+            <img src={data.photoURL ?? imgjpg} alt="photoURL" width="256" class="mx-auto" />
+            <p class="text-xl my-8">{data.bio ?? 'no bio yet...'}</p>
+            <ul class="list-none">
+                {#each data.links as item}
+                    <li>
+                        <UserLink {...item} />
+                    </li>
+                {/each}
+            </ul>
+</div>
+	<div class="flex flex-col w-[10vw]">
+        <Card class="bg-indigo-800">
+            <a href="/{data.username}/bio">
+                <h4 class="text-2xl text-white">Bio</h4>
+            </a>
+        </Card>
+        <Card class="bg-violet-800">
+            <a href="/{data.username}/edit">
+                <h4 class="text-2xl text-white">Edit</h4>
+            </a>
+        </Card>
+    </div>
+</main>
